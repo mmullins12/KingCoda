78b42280cd06e1fbe28d37333ca29e07a6c14512
mmullins12 <matthew.mullins@live.ca>
2021-11-06 23:18:18 -0400

    MODE - Words in File

---
+++ Ex9Definitions.py
@@ -0,0 +1,17 @@
+name = input('Enter File')
+handle = open(name, encoding='latin-1')
+
+counts = dict()
+for line in handle:
+    words = line.split()
+    for word in words:
+        counts[word] = counts.get(word,0) + 1
+
+bigCount = None
+bigWord = None
+for word,count in counts.items() :
+    if bigCount is None or count > bigCount:
+        bigWord = word
+        bigCount = count
+
+print(bigWord,bigCount)
